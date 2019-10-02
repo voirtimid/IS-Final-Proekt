15c55768f95c0596621d8f93da6c427e512550c8
Оливер Димитриов <oliver.dimitriov@students.finki.ukim.mk>
2019-10-02 12:32:17 +0200

    Final model

--- neural_network.py
+++ neural_network.py
@@ -11,6 +11,63 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from tqdm import tqdm
 
+from keras import metrics
+from matplotlib import pyplot
+import keras.backend as K
+
+def f1_score(y_true, y_pred):
+
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+
+    # How many selected items are relevant?
+    precision = c1 / c2
+    print("Precision: " + str(precision))
+
+    # How many relevant items are selected?
+    recall = c1 / c3
+    print("Recall: " + str(recall))
+
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
+
+def precision_score(y_true, y_pred):
+
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+
+    # How many selected items are relevant?
+    precision = c1 / c2
+    return precision
+
+def recall_score(y_true, y_pred):
+
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+
+    # How many relevant items are selected?
+    recall = c1 / c3
+    return recall
+
+
 train = pd.read_csv("dataset_csv.csv")
 
 train_image = []
@@ -50,9 +107,22 @@ model.add(Dense(256, activation='relu'))
 model.add(Dense(32, activation='softmax'))
 
 # Compile the model
-model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy', f1_score, precision_score, recall_score])
 
 # Training the model
-model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test))
 
 model.save("finalized_model.model")
+
+pyplot.subplot(211)
+pyplot.title('Precision')
+pyplot.plot(history.history['precision_score'], label='train')
+pyplot.plot(history.history['val_precision_score'], label='test')
+pyplot.legend()
+# plot accuracy during training
+pyplot.subplot(212)
+pyplot.title('Recall')
+pyplot.plot(history.history['recall_score'], label='train')
+pyplot.plot(history.history['val_recall_score'], label='test')
+pyplot.legend()
+pyplot.show()
