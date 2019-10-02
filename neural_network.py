080540f794c46faaa67ffd2522dbf9177c468d2e
Оливер Димитриов <oliver.dimitriov@students.finki.ukim.mk>
2019-10-02 12:47:50 +0200

    Final neural_network

--- neural_network.py
+++ neural_network.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 from keras import metrics
 from matplotlib import pyplot
 import keras.backend as K
+from sklearn.metrics import roc_curve, auc
 
 def f1_score(y_true, y_pred):
 
@@ -82,6 +83,7 @@ X = np.array(train_image)
 # Creating the target variable
 y=train["label"].values
 y = to_categorical(y)
+n_classes = y.shape[1]
 
 # Creating validation set
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
@@ -113,6 +115,111 @@ model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accurac
 history = model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test))
 
 model.save("finalized_model.model")
+
+letterMapping = {
+    1 : "А",
+    2 : "Б",
+    3 : "В",
+    4 : "Г",
+    5 : "Д",
+    6 : "Ѓ",
+    7 : "Е",
+    8 : "Ж",
+    9 : "З",
+    10 : "Ѕ",
+    11 : "И",
+    12 : "Ј",
+    13 : "К",
+    14 : "Л",
+    15 : "Љ",
+    16 : "М",
+    17 : "Н",
+    18 : "Њ",
+    19 : "О",
+    20 : "П",
+    21 : "Р",
+    22 : "С",
+    23 : "Т",
+    24 : "Ќ",
+    25 : "У",
+    26 : "Ф",
+    27 : "Х",
+    28 : "Ц",
+    29 : "Ч",
+    30 : "Џ",
+    31 : "Ш"
+}
+
+y_score = model.predict(X_test)
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+plt.figure()
+lw = 2
+plt.plot(fpr[2], tpr[2], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+plt.figure()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['orangered','deepskyblue','magenta','lawngreen','yellow','dodgerblue','blueviolet','red','lime','crimson','navy','darkcyan','maroon','orange','darkturquoise','aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes-1), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='{0} (area = {1:0.2f})'
+             ''.format(letterMapping[i+1], roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC')
+plt.legend(loc="lower right")
+plt.show()
 
 pyplot.subplot(211)
 pyplot.title('Precision')
